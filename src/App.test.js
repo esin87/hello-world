11d7daff135e4790b7fbbb0216014562040ced21
esin87 <esin.saribudak@gmail.com>
2021-03-25 20:24:34 -0500

    Add tests for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
+//https://blog.scottlogic.com/2020/09/03/create-test-integrate-and-deploy-a-react-app-in-under-10-minutes.html
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('should not show the message on mount', () => {
+	const wrapper = shallow(<App />);
+	expect(wrapper.find('.hello-message').exists()).toBeFalsy();
+});
+
+test('should show the correct button text on mount', () => {
+	const wrapper = shallow(<App />);
+	const button = wrapper.find('.hello-button').first();
+	expect(button.text()).toEqual('Say Hello');
+});
+
+test('should change the button text on click', () => {
+	const wrapper = shallow(<App />);
+	const button = wrapper.find('.hello-button').first();
+	button.simulate('click');
+	expect(wrapper.find('.hello-button').first().text()).toEqual('Reset');
+});
+
+test('should show the message on button click', () => {
+	const wrapper = shallow(<App />);
+	let button = wrapper.find('.hello-button').first();
+	button.simulate('click');
+	expect(wrapper.find('.hello-message').exists()).toBeTruthy();
 });
